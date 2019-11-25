6372a2d04f44475511bc1168b8ea46f5021c183a
Hitme Shop <hitmedotshop@gmail.com>
2019-11-25 22:15:16 +0600

    some route links added in index.html

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,80 +1,93 @@
-
-const products = require('../models/products')
+const products = require("../models/products");
 
 const defaultError = (res, error) => {
-   res.json({ error: true, message: error.message })
-}
+   res.json({ error: true, message: error.message });
+};
 
 exports.getAll = async (_, res) => {
    try {
-      let projection = "title src url sPrice oPrice discount rating ratingCount website flag cat"
-      let docRes = await products.find({}, projection)
-      res.json({ results: docRes.length, data: docRes })
-   } catch (error) { defaultError(res, error) }
-}
+      let projection =
+         "_id title src url sPrice oPrice discount rating ratingCount website flag cat";
+      let docRes = await products.find({}, projection);
+      res.json({ results: docRes.length, data: docRes });
+   } catch (error) {
+      defaultError(res, error);
+   }
+};
 
 exports.create = async (req, res) => {
    try {
-      await products.init()
-      let docRes = await products.create(req.body)
-      res.json(docRes)
-   } catch (error) { defaultError(res, { message: "DUPLICATE_PRODUCT" }) }
-}
+      await products.init();
+      let docRes = await products.create(req.body);
+      res.json(docRes);
+   } catch (error) {
+      defaultError(res, { message: "DUPLICATE_PRODUCT" });
+   }
+};
 
 exports.get = async (req, res) => {
    try {
-      let docRes = await products.findOne({ _id: req.params.id })
-      res.json(docRes)
-   } catch (error) { defaultError(res, error) }
-}
+      let docRes = await products.findOne({ _id: req.params.id });
+      res.json(docRes);
+   } catch (error) {
+      defaultError(res, error);
+   }
+};
 exports.update = async (_, res) => {
    try {
-      res.json("patch")
-   } catch (error) { defaultError(res, error) }
-}
+      res.json("patch");
+   } catch (error) {
+      defaultError(res, error);
+   }
+};
 
 exports.delete = async (req, res) => {
    try {
-      let docRes = await products.deleteOne({ _id: req.params.id })
-      res.json(docRes)
-   } catch (error) { defaultError(res, error) }
-}
+      let docRes = await products.deleteOne({ _id: req.params.id });
+      res.json(docRes);
+   } catch (error) {
+      defaultError(res, error);
+   }
+};
 
 exports.deleteAll = async (_, res) => {
    try {
-      let docRes = await products.deleteMany({})
-      res.json(docRes)
-   } catch (error) { defaultError(res, error) }
-}
+      let docRes = await products.deleteMany({});
+      res.json(docRes);
+   } catch (error) {
+      defaultError(res, error);
+   }
+};
 
 exports.compareAll = async (_, res) => {
    try {
-
-      let proCount = await products.countDocuments()
+      let proCount = await products.countDocuments();
       console.log(proCount);
-      let page = 1
-      let limit = 100
+      let page = 1;
+      let limit = 100;
 
-      console.log(proCount/limit);
-   
+      console.log(proCount / limit);
+
       // while ( proCount / 100 ) {
-         
-      // }
-      
-      res.json('Compare All')
 
-   } catch (error) { defaultError(res, error) }
-}
+      // }
+
+      res.json("Compare All");
+   } catch (error) {
+      defaultError(res, error);
+   }
+};
 
 exports.compare = async (req, res) => {
    try {
-
       console.log(req.params.title);
 
-      (await products.find()).forEach(function (pro) {
+      (await products.find()).forEach(function(pro) {
          console.log(pro);
-      })
+      });
 
-      res.json('docRes')
-   } catch (error) { defaultError(res, error) }
-}
+      res.json("docRes");
+   } catch (error) {
+      defaultError(res, error);
+   }
+};
